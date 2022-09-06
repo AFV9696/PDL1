adf302c96662713d01ec357f687cacd85e921a9b
AFV9696 <112088757+AFV9696@users.noreply.github.com>
2022-09-05 21:57:37 -0500

    Cambio 1.1

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int radio; 
+
+    cout << "Introduzca el radio del circulo: ";
+    cin >> radio;
+    
+    cout << "El diametro es " << radio * 2.0
+    << "\nLa circunferencia es " << 2 * 3.14159 * radio
+    << "\nEl area es " << 3.14159 * radio * radio << endl;
+
+ return 0;
+}
